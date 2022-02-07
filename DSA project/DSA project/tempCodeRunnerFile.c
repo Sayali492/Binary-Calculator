2daa3aa2076a0d2c6922478d87b43db97a9964fb
Sayali492 <85893942+Sayali492@users.noreply.github.com>
2022-02-07 16:15:31 +0530

    Updated division function

---
+++ DSA project/DSA project/tempCodeRunnerFile.c
@@ -0,0 +1,8 @@
+ // printf("Subtraction\n");
+                    // node *ptr1 = diff;
+                    // while (ptr1 != NULL)
+                    // {
+                    //           printf("%d %d\n", ptr1->data, ptr1->index);
+                    //           ptr1 = ptr1->next;
+                    // }
+                    // printf("\n");
